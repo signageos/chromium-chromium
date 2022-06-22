ca1c5a16d782c4905bf84fb165346a4a077d2e24
Eugen Pechanec <e.pechanec@gmail.com>
2022-06-23 00:04:30 +0200

    Hotfix AwVariationsSeedFetcher for Kitkat

--- android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedHolder.java
+++ android_webview/nonembedded/java/src/org/chromium/android_webview/services/VariationsSeedHolder.java
@@ -4,6 +4,7 @@
 
 package org.chromium.android_webview.services;
 
+import android.os.Build;
 import android.os.Handler;
 import android.os.HandlerThread;
 import android.os.ParcelFileDescriptor;
@@ -145,7 +146,11 @@ public class VariationsSeedHolder {
 
     @VisibleForTesting
     public void scheduleFetchIfNeeded() {
-        AwVariationsSeedFetcher.scheduleIfNeeded();
+        if (Build.VERSION.SDK_INT >= 21) {
+            AwVariationsSeedFetcher.scheduleIfNeeded();
+        } else {
+            AwVariationsSeedFetcherService.scheduleIfNeeded();
+        }
     }
 
     // overridden by tests
