9f9fb3c1d1ca7a2a9686daa1bdaae5fafd4b1e49
Torne (Richard Coles) <torne@google.com>
2023-03-13 17:33:36 +0000

    android: clean up checking FLAG_DEBUGGABLE.
    Various places in the code check if the current app is debuggable; move
    this to a common implementation in BuildInfo to simplify the logic.
    
    Change-Id: I6eca8f79da97527f0cd93f1a4e52557f41e05d01
    Reviewed-on: https://chromium-review.googlesource.com/c/chromium/src/+/4327284
    Reviewed-by: Andrew Grieve <agrieve@chromium.org>
    Reviewed-by: Rayan Kanso <rayankans@chromium.org>
    Commit-Queue: Richard Coles <torne@chromium.org>
    Auto-Submit: Richard Coles <torne@chromium.org>
    Cr-Commit-Position: refs/heads/main@{#1116465}

--- android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
+++ android_webview/glue/java/src/com/android/webview/chromium/WebViewChromiumFactoryProvider.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.ContextWrapper;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageInfo;
 import android.content.res.AssetManager;
 import android.net.Uri;
@@ -392,12 +391,9 @@ public class WebViewChromiumFactoryProvider implements WebViewFactoryProvider {
                 cl.appendSwitch(AwSwitches.WEBVIEW_ENABLE_MODERN_COOKIE_SAME_SITE);
             }
 
-            int applicationFlags = ctx.getApplicationInfo().flags;
-            boolean isAppDebuggable = (applicationFlags & ApplicationInfo.FLAG_DEBUGGABLE) != 0;
-            boolean isOsDebuggable = BuildInfo.isDebugAndroid();
             // Enable logging JS console messages in system logs only if the app is debuggable or
-            // it's a debugable android build.
-            if (isAppDebuggable || isOsDebuggable) {
+            // it's a debuggable android build.
+            if (BuildInfo.isDebugAndroidOrApp()) {
                 CommandLine cl = CommandLine.getInstance();
                 cl.appendSwitch(AwSwitches.WEBVIEW_LOG_JS_CONSOLE_MESSAGES);
             }
