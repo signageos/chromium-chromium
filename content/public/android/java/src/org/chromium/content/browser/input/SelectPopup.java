ed67402e6d95af52bd918f866c4dcea94c031925
Eugen Pechanec <e.pechanec@gmail.com>
2024-01-19 00:43:15 +0100

    Fix select popup on Android 4.4
    getDrawable works fine but obtainStyledAttributes
    doesn't resolve resource IDs and resource paths.
    
    This only happens on tablets when using drop downs popups.
    Forcing dialog popups on API < 21 works around the issue.
    
    android.content.res.Resources$NotFoundException: File res/drawable/popup_bg_tinted.xml from drawable resource ID #0x70084
    Caused by: org.xmlpull.v1.XmlPullParserException: Binary XML file line #9: <item> tag requires a 'drawable' attribute or child tag defining a drawable

--- content/public/android/java/src/org/chromium/content/browser/input/SelectPopup.java
+++ content/public/android/java/src/org/chromium/content/browser/input/SelectPopup.java
@@ -7,6 +7,7 @@ package org.chromium.content.browser.input;
 import android.content.Context;
 import android.view.View;
 import android.view.ViewGroup;
+import android.os.Build;
 
 import androidx.annotation.VisibleForTesting;
 
@@ -152,7 +153,8 @@ public class SelectPopup implements HideablePopup, ViewAndroidDelegate.Container
         }
         WebContentsAccessibilityImpl wcax =
                 WebContentsAccessibilityImpl.fromWebContents(mWebContents);
-        if (DeviceFormFactor.isTablet() && !multiple && !wcax.isTouchExplorationEnabled()) {
+        if (Build.VERSION.SDK_INT >= 21 &&
+                DeviceFormFactor.isTablet() && !multiple && !wcax.isTouchExplorationEnabled()) {
             mPopupView = new SelectPopupDropdown(context, this::selectMenuItems, anchorView,
                     popupItems, selectedIndices, rightAligned, mWebContents);
         } else {
