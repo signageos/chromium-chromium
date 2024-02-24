c8ba35e11a60a566aca14be112ec2f101e49309f
Eugen Pechanec <e.pechanec@gmail.com>
2024-02-24 03:08:21 +0100

    Revert "Remove UsingVizForWebView usage from blink"
    This reverts commit fea519bae4dfba049058ef11423b693c2b0bea37.

--- android_webview/browser/aw_settings.cc
+++ android_webview/browser/aw_settings.cc
@@ -43,6 +43,8 @@ void PopulateFixedWebPreferences(WebPreferences* web_prefs) {
   web_prefs->should_clear_document_background = false;
   web_prefs->viewport_meta_enabled = true;
   web_prefs->picture_in_picture_enabled = false;
+  web_prefs->disable_features_depending_on_viz =
+      !::features::IsUsingVizForWebView();
   web_prefs->disable_accelerated_small_canvases = true;
   // WebView has historically not adjusted font scale for text autosizing.
   web_prefs->device_scale_adjustment = 1.0;
