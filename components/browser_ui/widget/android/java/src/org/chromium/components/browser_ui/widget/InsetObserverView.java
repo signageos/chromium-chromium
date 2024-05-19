d87a37504d351e6c85129987c8d0930ffee7480d
Eugen Pechanec <e.pechanec@gmail.com>
2024-05-19 17:04:52 +0200

    Revert "Android: Remove InsetObserverViewApi21"
    This reverts commit 05617d7e

--- components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/InsetObserverView.java
+++ components/browser_ui/widget/android/java/src/org/chromium/components/browser_ui/widget/InsetObserverView.java
@@ -108,6 +108,8 @@ public class InsetObserverView extends View {
     public static InsetObserverView create(Context context) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
             return new InsetObserverViewApi28(context);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            return new InsetObserverViewApi21(context);
         }
         return new InsetObserverView(context);
     }
@@ -245,13 +247,15 @@ public class InsetObserverView extends View {
         return mWindowInsetsAnimationProxyCallback;
     }
 
+    @SuppressWarnings("deprecation")
     @Override
-    public WindowInsets onApplyWindowInsets(WindowInsets insets) {
-        insets = forwardToInsetConsumers(insets);
-        updateKeyboardInset();
-        onInsetChanged(insets.getSystemWindowInsetLeft(), insets.getSystemWindowInsetTop(),
-                insets.getSystemWindowInsetRight(), insets.getSystemWindowInsetBottom());
-        return insets;
+    protected boolean fitSystemWindows(Rect insets) {
+        // For Lollipop and above, onApplyWindowInsets will set the insets.
+        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.LOLLIPOP) {
+            updateKeyboardInset();
+            onInsetChanged(insets.left, insets.top, insets.right, insets.bottom);
+        }
+        return false;
     }
 
     /**
@@ -290,17 +294,37 @@ public class InsetObserverView extends View {
         }
     }
 
-    private WindowInsets forwardToInsetConsumers(WindowInsets insets) {
-        WindowInsetsCompat windowInsetsCompat =
-                WindowInsetsCompat.toWindowInsetsCompat(insets, this);
-        for (WindowInsetsConsumer consumer : mInsetsConsumers) {
-            windowInsetsCompat = consumer.onApplyWindowInsets(this, windowInsetsCompat);
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    private static class InsetObserverViewApi21 extends InsetObserverView {
+        /**
+         * Creates an instance of {@link InsetObserverView} for Android versions L and above.
+         * @param context The Context to create this {@link InsetObserverView} in.
+         */
+        InsetObserverViewApi21(Context context) {
+            super(context);
         }
-        return windowInsetsCompat.toWindowInsets();
+
+        @Override
+        public WindowInsets onApplyWindowInsets(WindowInsets insets) {
+            insets = forwardToInsetConsumers(insets);
+            updateKeyboardInset();
+            onInsetChanged(insets.getSystemWindowInsetLeft(), insets.getSystemWindowInsetTop(),
+                    insets.getSystemWindowInsetRight(), insets.getSystemWindowInsetBottom());
+            return insets;
+        }
+
+        private WindowInsets forwardToInsetConsumers(WindowInsets insets) {
+            WindowInsetsCompat windowInsetsCompat =
+                    WindowInsetsCompat.toWindowInsetsCompat(insets, this);
+            for (WindowInsetsConsumer consumer : mInsetsConsumers) {
+                windowInsetsCompat = consumer.onApplyWindowInsets(this, windowInsetsCompat);
+            }
+            return windowInsetsCompat.toWindowInsets();
+        }
     }
 
     @RequiresApi(Build.VERSION_CODES.P)
-    private static class InsetObserverViewApi28 extends InsetObserverView {
+    private static class InsetObserverViewApi28 extends InsetObserverViewApi21 {
         private Rect mCurrentSafeArea = new Rect();
 
         /**
