a9b09b4e6abc5fc290656afaa21218e65b1227d3
Eugen Pechanec <e.pechanec@gmail.com>
2024-05-19 19:30:41 +0200

    Revert "Remove KitKat-only code from //content" (input)
    This *partially* reverts commit cc1bf8ea

--- content/public/android/javatests/src/org/chromium/content/browser/input/CursorAnchorInfoControllerTest.java
+++ content/public/android/javatests/src/org/chromium/content/browser/input/CursorAnchorInfoControllerTest.java
@@ -18,6 +18,7 @@ import org.junit.runner.RunWith;
 
 import org.chromium.base.test.util.Batch;
 import org.chromium.base.test.util.Feature;
+import org.chromium.base.test.util.MinAndroidSdkLevel;
 import org.chromium.content_public.browser.test.ContentJUnit4ClassRunner;
 import org.chromium.content_public.browser.test.util.TestInputMethodManagerWrapper;
 
@@ -26,6 +27,7 @@ import org.chromium.content_public.browser.test.util.TestInputMethodManagerWrapp
  */
 @RunWith(ContentJUnit4ClassRunner.class)
 @Batch(Batch.UNIT_TESTS)
+@MinAndroidSdkLevel(Build.VERSION_CODES.LOLLIPOP)
 public class CursorAnchorInfoControllerTest {
     private static final class TestViewDelegate implements CursorAnchorInfoController.ViewDelegate {
         public int locationX;
