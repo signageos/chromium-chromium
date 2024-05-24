5eab338598c016d8fe5f7213e06729744813f2c2
Eugen Pechanec <e.pechanec@gmail.com>
2024-05-24 23:31:31 +0200

    Revert "[Code Health] Clean up some pre-Nougat code in media/ code"
    This reverts commit 737bc922

--- media/midi/midi_manager_android.cc
+++ media/midi/midi_manager_android.cc
@@ -4,6 +4,7 @@
 
 #include "media/midi/midi_manager_android.h"
 
+#include "base/android/build_info.h"
 #include "base/feature_list.h"
 #include "base/functional/bind.h"
 #include "base/metrics/field_trial_params.h"
@@ -26,6 +27,10 @@ namespace midi {
 namespace {
 
 bool HasSystemFeatureMidi() {
+  // MIDI API was added at Android M.
+  DCHECK_GE(base::android::BuildInfo::GetInstance()->sdk_int(),
+            base::android::SDK_VERSION_MARSHMALLOW);
+
   // Check if the MIDI service actually runs on the system.
   return Java_MidiManagerAndroid_hasSystemFeatureMidi(
       base::android::AttachCurrentThread());
