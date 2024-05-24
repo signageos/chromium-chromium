fef9922bcff31d15b5bb9a51dce3516e6a0c734c
Eugen Pechanec <e.pechanec@gmail.com>
2024-05-24 22:54:52 +0200

    Revert "[Code Health] Clean up some pre-Nougat code"
    This reverts commit 4b955632b67c7387c89831f7867307d41b3dcf11.

--- components/permissions/permission_manager_unittest.cc
+++ components/permissions/permission_manager_unittest.cc
@@ -35,6 +35,10 @@
 #include "third_party/blink/public/mojom/permissions_policy/permissions_policy.mojom.h"
 #include "url/origin.h"
 
+#if BUILDFLAG(IS_ANDROID)
+#include "base/android/build_info.h"
+#endif  // BUILDFLAG(IS_ANDROID)
+
 using blink::PermissionType;
 using blink::mojom::PermissionsPolicyFeature;
 
@@ -68,6 +72,23 @@ class ScopedPartitionedOriginBrowserClient
   url::Origin app_origin_;
   raw_ptr<content::ContentBrowserClient> old_client_;
 };
+
+#if BUILDFLAG(IS_ANDROID)
+// See https://crbug.com/904883.
+auto GetDefaultProtectedMediaIdentifierPermissionStatus() {
+  return base::android::BuildInfo::GetInstance()->sdk_int() >=
+                 base::android::SDK_VERSION_MARSHMALLOW
+             ? PermissionStatus::GRANTED
+             : PermissionStatus::ASK;
+}
+
+auto GetDefaultProtectedMediaIdentifierContentSetting() {
+  return base::android::BuildInfo::GetInstance()->sdk_int() >=
+                 base::android::SDK_VERSION_MARSHMALLOW
+             ? PermissionStatus::GRANTED
+             : PermissionStatus::ASK;
+}
+#endif  // BUILDFLAG(IS_ANDROID)
 
 }  // namespace
 
@@ -330,7 +351,7 @@ TEST_F(PermissionManagerTest, GetPermissionStatusDefault) {
   CheckPermissionStatus(PermissionType::GEOLOCATION, PermissionStatus::ASK);
 #if BUILDFLAG(IS_ANDROID)
   CheckPermissionStatus(PermissionType::PROTECTED_MEDIA_IDENTIFIER,
-                        PermissionStatus::GRANTED);
+                        GetDefaultProtectedMediaIdentifierPermissionStatus());
   CheckPermissionStatus(PermissionType::WINDOW_MANAGEMENT,
                         PermissionStatus::DENIED);
 #else
@@ -375,7 +396,7 @@ TEST_F(PermissionManagerTest, CheckPermissionResultDefault) {
                         content::PermissionStatusSource::UNSPECIFIED);
 #if BUILDFLAG(IS_ANDROID)
   CheckPermissionResult(PermissionType::PROTECTED_MEDIA_IDENTIFIER,
-                        PermissionStatus::GRANTED,
+                        GetDefaultProtectedMediaIdentifierContentSetting(),
                         content::PermissionStatusSource::UNSPECIFIED);
 #endif
 }
