9238dabb1972b4de3b7b3fcf4ddec59be52ce1e2
Eugen Pechanec <e.pechanec@gmail.com>
2024-05-24 23:45:15 +0200

    Revert "[Code Health] Clean up some pre-Nougat code in media/ code"
    This reverts commit 790212c7

--- media/base/android/java/src/org/chromium/media/MediaDrmStorageBridge.java
+++ media/base/android/java/src/org/chromium/media/MediaDrmStorageBridge.java
@@ -4,6 +4,10 @@
 
 package org.chromium.media;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
 import org.chromium.base.Callback;
 import org.chromium.base.annotations.CalledByNative;
 import org.chromium.base.annotations.JNINamespace;
@@ -16,6 +20,7 @@ import org.chromium.build.annotations.MainDex;
  */
 @JNINamespace("media")
 @MainDex
+@RequiresApi(Build.VERSION_CODES.M)
 class MediaDrmStorageBridge {
     private static final long INVALID_NATIVE_MEDIA_DRM_STORAGE_BRIDGE = -1;
 
