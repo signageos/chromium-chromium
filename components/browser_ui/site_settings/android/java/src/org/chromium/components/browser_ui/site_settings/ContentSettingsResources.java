6f1e3ab6a4595fd2bcb39e91dd2bc5a8f96834d1
Eugen Pechanec <e.pechanec@gmail.com>
2024-05-24 18:22:57 +0200

    Revert "[Android] Remove L- code in ContentSettingsResources"
    This reverts commit 8e7a295363e6dbed6a2820237bfb1d8ea6553240.

--- components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/ContentSettingsResources.java
+++ components/browser_ui/site_settings/android/java/src/org/chromium/components/browser_ui/site_settings/ContentSettingsResources.java
@@ -16,6 +16,7 @@ import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.os.Build;
 
 import androidx.annotation.Nullable;
 
@@ -610,11 +611,21 @@ public class ContentSettingsResources {
      */
     public static int[] getTriStateSettingDescriptionIDs(int contentType) {
         if (contentType == ContentSettingsType.PROTECTED_MEDIA_IDENTIFIER) {
-            int[] descriptionIDs = {
-                    R.string.website_settings_category_protected_content_allowed_recommended,
-                    R.string.website_settings_category_protected_content_ask,
-                    R.string.website_settings_category_protected_content_blocked};
-            return descriptionIDs;
+            // The recommended setting is different on different android versions depending on
+            // whether per-origin provisioning is available. See https://crbug.com/904883.
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                int[] descriptionIDs = {
+                        R.string.website_settings_category_protected_content_allowed_recommended,
+                        R.string.website_settings_category_protected_content_ask,
+                        R.string.website_settings_category_protected_content_blocked};
+                return descriptionIDs;
+            } else {
+                int[] descriptionIDs = {
+                        R.string.website_settings_category_protected_content_allowed,
+                        R.string.website_settings_category_protected_content_ask_recommended,
+                        R.string.website_settings_category_protected_content_blocked};
+                return descriptionIDs;
+            }
         }
 
         assert false;
