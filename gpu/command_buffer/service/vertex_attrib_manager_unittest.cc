ed49203a6aada97c20a5756a0792cb5ad36339b8
Eugen Pechanec <e.pechanec@gmail.com>
2024-05-28 13:53:53 +0200

    Revert "Remove GPU workarounds and entries."
    This reverts commit 5eec641267a4f6da4d92d4c3a315c2c3b12c8a2f.

--- gpu/command_buffer/service/vertex_attrib_manager_unittest.cc
+++ gpu/command_buffer/service/vertex_attrib_manager_unittest.cc
@@ -32,8 +32,14 @@ class VertexAttribManagerTest : public GpuServiceTest {
   void SetUp() override {
     GpuServiceTest::SetUp();
 
+    for (uint32_t ii = 0; ii < kNumVertexAttribs; ++ii) {
+      EXPECT_CALL(*gl_, VertexAttrib4f(ii, 0.0f, 0.0f, 0.0f, 1.0f))
+          .Times(1)
+          .RetiresOnSaturation();
+    }
+
     manager_ = new VertexAttribManager(false);
-    manager_->Initialize(kNumVertexAttribs);
+    manager_->Initialize(kNumVertexAttribs, true);
     manager_->SetIsBound(true);
   }
 
--- gpu/config/gpu_driver_bug_list_unittest.cc
+++ gpu/config/gpu_driver_bug_list_unittest.cc
@@ -50,7 +50,7 @@ TEST_F(GpuDriverBugListTest, AppendSingleWorkaround) {
       DISABLE_CHROMIUM_FRAMEBUFFER_MULTISAMPLE));
   std::set<int> workarounds;
   workarounds.insert(EXIT_ON_CONTEXT_LOST);
-  workarounds.insert(GL_CLEAR_BROKEN);
+  workarounds.insert(INIT_VERTEX_ATTRIBUTES);
   EXPECT_EQ(2u, workarounds.size());
   GpuDriverBugList::AppendWorkaroundsFromCommandLine(
       &workarounds, command_line);
