7f716ed166a802bb644770f542381de98b132bfd
Eugen Pechanec <e.pechanec@gmail.com>
2024-05-30 08:00:34 +0200

    Revert "Deprecate Android Lollipop support in Bluetooth adapter"
    This reverts commit cd44c5d7

--- device/bluetooth/bluetooth_adapter_factory.cc
+++ device/bluetooth/bluetooth_adapter_factory.cc
@@ -23,6 +23,9 @@
 #include "base/win/windows_version.h"
 #include "device/bluetooth/bluetooth_adapter_win.h"
 #endif
+#if defined(ANDROID)
+#include "base/android/build_info.h"
+#endif
 
 namespace device {
 
@@ -55,8 +58,10 @@ bool BluetoothAdapterFactory::IsLowEnergySupported() {
     return values_for_testing_->GetLESupported();
   }
 
-#if BUILDFLAG(IS_ANDROID) || BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || \
-    BUILDFLAG(IS_MAC)
+#if defined(OS_ANDROID)
+  return base::android::BuildInfo::GetInstance()->sdk_int() >=
+         base::android::SDK_VERSION_MARSHMALLOW;
+#elif BUILDFLAG(IS_CHROMEOS) || BUILDFLAG(IS_LINUX) || BUILDFLAG(IS_MAC)
   return true;
 #elif BUILDFLAG(IS_WIN)
   // Windows 8 supports Low Energy GATT operations but it does not support
